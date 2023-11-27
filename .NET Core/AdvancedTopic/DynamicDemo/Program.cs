8f1c808d508c1ba2a24ff23e186099437ddd3863
John Li <lijunleejohN@gmail.com>
2023-11-27 15:54:25 -0500

    OK

---
+++ .NET Core/AdvancedTopic/DynamicDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DynamicExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleClass ec = new ExampleClass();
+            Console.WriteLine(ec.ExampleMethod(10));
+            Console.WriteLine(ec.ExampleMethod("value"));
+
+            // The following line causes a compiler error because ExampleMethod
+            // takes only one argument.
+            //Console.WriteLine(ec.ExampleMethod(10, 4));
+
+            dynamic dynamic_ec = new ExampleClass();
+            Console.WriteLine(dynamic_ec.ExampleMethod(10));
+
+            // Because dynamic_ec is dynamic, the following call to ExampleMethod
+            // with two arguments does not produce an error at compile time.
+            // However, it does cause a run-time error.
+            //Console.WriteLine(dynamic_ec.ExampleMethod(10, 4));
+        }
+    }
+
+    class ExampleClass
+    {
+        static dynamic _field;
+        dynamic Prop { get; set; }
+
+        public dynamic ExampleMethod(dynamic d)
+        {
+            dynamic local = "Local variable";
+            int two = 2;
+
+            if (d is int)
+            {
+                return local;
+            }
+            else
+            {
+                return two;
+            }
+        }
+    }
+}
+// Results:
+// Local variable
+// 2
+// Local variable
